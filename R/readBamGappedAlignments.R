2c407a236b0806fb8bc8de574e350f9f35b66447
Martin Morgan <mtmorgan@fhcrc.org>
2011-04-28 00:08:00 +0000

    catch invalid index file when scanning portions of bam files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_8/madman/Rpacks/Rsamtools@55110 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readBamGappedAlignments.R
+++ R/readBamGappedAlignments.R
@@ -1,12 +1,12 @@
 setMethod(readBamGappedAlignments, "character",
           function(file, index, ..., which)
 {
-    if (missing(index)) {
-        tmp <- paste(.normalizePath(file), "bai", sep=".")
-        index <- if (file.exists(tmp)) file else character(0)
-    }
     if (missing(which))
         which <- RangesList()
+    if (missing(index))
+        index <-
+            if (0L == length(which)) character()
+            else file
     bam <- open(BamFile(file, index), "rb")
     on.exit(close(bam))
     callGeneric(bam, character(), ..., which=which)
